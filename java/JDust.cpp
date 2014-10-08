b09354ab56cbb74e23b7ac4c8e6b9317a61c1ef7
Tom Alexander <craftkiller@gmail.com>
2014-10-08 00:49:27 -0400

    Java ported to new API

--- java/JDust.cpp
+++ java/JDust.cpp
@@ -8,13 +8,56 @@
 
 extern "C"
 JNIEXPORT jint JNICALL
-Java_buzz_fizz_jdust_JDust_executeJs(JNIEnv* env, jobject callingObject, jobject state, jstring js)
+Java_buzz_fizz_jdust_JDust_compileTemplate(JNIEnv* env, jobject callingObject, jobject state, jstring fullDust, jstring dustTemplate, jstring templateName)
 {
-    const char *native_js = env->GetStringUTFChars(js, 0);
+    const char* cFullDust = env->GetStringUTFChars(fullDust, 0);
+    const char* cDustTemplate = env->GetStringUTFChars(dustTemplate, 0);
+    const char* cTemplateName = env->GetStringUTFChars(templateName, 0);
+
 
     JStore jstore(env, state);
-    int ret = execute_js(jstore, native_js);
+    int ret = compile_template(jstore, cFullDust, cDustTemplate, cTemplateName);
 
-    env->ReleaseStringUTFChars(js, native_js);
+    env->ReleaseStringUTFChars(fullDust, cFullDust);
+    env->ReleaseStringUTFChars(dustTemplate, cDustTemplate);
+    env->ReleaseStringUTFChars(templateName, cTemplateName);
+    return ret;
+}
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_buzz_fizz_jdust_JDust_renderCompiledTemplate(JNIEnv* env, jobject callingObject, jobject state, jstring coreDust, jstring compiledTemplate, jstring dustContext, jstring templateName)
+{
+    const char* cCoreDust = env->GetStringUTFChars(coreDust, 0);
+    const char* cCompiledTemplate = env->GetStringUTFChars(compiledTemplate, 0);
+    const char* cDustContext = env->GetStringUTFChars(dustContext, 0);
+    const char* cTemplateName = env->GetStringUTFChars(templateName, 0);
+
+
+    JStore jstore(env, state);
+    int ret = render_compiled_template(jstore, cCoreDust, cCompiledTemplate, cDustContext, cTemplateName);
+
+    env->ReleaseStringUTFChars(coreDust, cCoreDust);
+    env->ReleaseStringUTFChars(compiledTemplate, cCompiledTemplate);
+    env->ReleaseStringUTFChars(templateName, cTemplateName);
+    env->ReleaseStringUTFChars(dustContext, cDustContext);
+    return ret;
+}
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_buzz_fizz_jdust_JDust_renderTemplate(JNIEnv* env, jobject callingObject, jobject state, jstring fullDust, jstring dustTemplate, jstring dustContext)
+{
+    const char* cFullDust = env->GetStringUTFChars(fullDust, 0);
+    const char* cDustTemplate = env->GetStringUTFChars(dustTemplate, 0);
+    const char* cDustContext = env->GetStringUTFChars(dustContext, 0);
+
+
+    JStore jstore(env, state);
+    int ret = render_template(jstore, cFullDust, cDustTemplate, cDustContext);
+
+    env->ReleaseStringUTFChars(fullDust, cFullDust);
+    env->ReleaseStringUTFChars(dustTemplate, cDustTemplate);
+    env->ReleaseStringUTFChars(dustContext, cDustContext);
     return ret;
 }
--- java/buzz/fizz/jdust/JDust.java
+++ java/buzz/fizz/jdust/JDust.java
@@ -12,32 +12,33 @@ import java.nio.file.Paths;
 public class JDust
 {
 
-    private native int executeJs(JState state, String js);
+    private native int compileTemplate(JState state, String fullDust, String dustTemplate, String templateName);
+    private native int renderCompiledTemplate(JState state, String coreDust, String compiledTemplate, String dustContext, String templateName);
+    private native int renderTemplate(JState state, String fullDust, String dustTemplate, String dustContext);
+    
+    private static String readFile(String path)
+    {
+        Path p = Paths.get(path);
+        try 
+        {
+            byte[] rawData = Files.readAllBytes(p);
+            return new String(rawData);
+        } catch (IOException e) {
+            System.out.println("Error loading " + path);
+        }
+        return "";
+    }
 
     public static void main(String[] args)
     {
-        String out = new JDust().renderDust(args);
+        String out = new JDust().renderDust(readFile(args[0]), readFile(args[1]), "{'title': 'Hello Title'}");
         System.out.println(out);
     }
     
-    private String renderDust(String[] files)
+    private String renderDust(String fullDust, String dustTemplate, String dustContext)
     {
         JState state = new JState();
-        StringBuilder buffer = new StringBuilder();
-        for (String f : files)
-        {
-            Path p = Paths.get(f);
-            try 
-            {
-                byte[] data = Files.readAllBytes(p);
-                String sData = new String(data);
-                buffer.append(sData);
-                
-            } catch (IOException e) {
-                System.out.println("Error loading " + f);
-            }
-        }
-        int status = executeJs(state, buffer.toString());
+        int status = renderTemplate(state, fullDust, dustTemplate, dustContext);
         return state.getOut();
     }
 
