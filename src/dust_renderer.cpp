12224c10a5ef8091825af8a16f3df2dfb81a88d9
Tom Alexander <craftkiller@gmail.com>
2014-10-09 22:38:22 -0400

    fixed memory leak

--- src/dust_renderer.cpp
+++ src/dust_renderer.cpp
@@ -19,37 +19,41 @@ namespace
     {
         // Create a new Isolate and make it the current one.
         Isolate* isolate = Isolate::New();
-        Isolate::Scope isolate_scope(isolate);
-
-        // Create a stack-allocated handle scope.
-        HandleScope handle_scope(isolate);
-
-        // Create a template for the global object.
-        Local<ObjectTemplate> global = ObjectTemplate::New(isolate);
-
-        // Create a new context.
-        Local<Context> context = Context::New(isolate, nullptr, global);
-
-        // Enter the context for compiling and running the hello world script.
-        Context::Scope context_scope(context);
-
-        Local<Object> v8_state = wrap_out_streams(isolate, &out);
-        context->Global()->Set(String::NewFromUtf8(isolate, "console"), v8_state);
-
-        for (auto const & entry : global_vars)
         {
-            Handle<String> current_var = String::NewFromUtf8(isolate, entry.second.c_str());
-            context->Global()->Set(String::NewFromUtf8(isolate, entry.first.c_str()), current_var);
+            Isolate::Scope isolate_scope(isolate);
+
+            // Create a stack-allocated handle scope.
+            HandleScope handle_scope(isolate);
+
+            // Create a template for the global object.
+            Local<ObjectTemplate> global = ObjectTemplate::New(isolate);
+
+            // Create a new context.
+            Local<Context> context = Context::New(isolate, nullptr, global);
+
+            // Enter the context for compiling and running the hello world script.
+            Context::Scope context_scope(context);
+
+            Local<Object> v8_state = wrap_out_streams(isolate, &out);
+            context->Global()->Set(String::NewFromUtf8(isolate, "console"), v8_state);
+
+            for (auto const & entry : global_vars)
+            {
+                Handle<String> current_var = String::NewFromUtf8(isolate, entry.second.c_str());
+                context->Global()->Set(String::NewFromUtf8(isolate, entry.first.c_str()), current_var);
+            }
+
+            // Create a string containing the JavaScript source code.
+            Local<String> source = String::NewFromUtf8(isolate, raw_source.c_str());
+
+            // Compile the source code.
+            Local<Script> script = Script::Compile(source);
+
+            // Run the script to get the result.
+            Local<Value> result = script->Run();
         }
 
-        // Create a string containing the JavaScript source code.
-        Local<String> source = String::NewFromUtf8(isolate, raw_source.c_str());
-
-        // Compile the source code.
-        Local<Script> script = Script::Compile(source);
-
-        // Run the script to get the result.
-        Local<Value> result = script->Run();
+        isolate->Dispose();
 
         return SUCCESS;
     }
